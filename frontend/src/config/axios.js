e0e10c240b86d92dfb5385897a07f96421df55fd
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2024-01-01 21:00:06 +0530

    configured axios

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,59 +1,65 @@
-import _axios from "axios";
+import axios from "axios";
 
-if (import.meta.env.MODE != "development") {
-    console.log("Is prod.");
-} else {
-    console.log("Is dev.");
-}
+// if (import.meta.env.MODE != "development") {
+//     console.log("Is prod.");
+// } else {
+//     console.log("Is dev.");
+// }
 
-const axios = _axios.create({
-    baseURL:
-        import.meta.env.MODE != "development"
-            ? "/api"
-            : "https://mindfulbackend.azurewebsites.net/api",
-    timeout: 3000,
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-    },
-});
+// let BASE
 
-axios.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        if (token) config.headers.Authorization = `Bearer ${token}`;
-        return config;
-    },
-    (error) => {
-        // Do something with request error
-        return Promise.reject(error);
-    }
-);
+// const axios = _axios.create({
+//     baseURL:
+//         import.meta.env.MODE != "development"
+//             ? "/api"
+//             : "https://mindfulbackend.azurewebsites.net/api",
+//     timeout: 3000,
+//     headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//     },
+// });
 
-axios.interceptors.response.use(
-    (response) => {
-        return response.data;
-    },
-    (error) => {
-        if (error.code == "ECONNABORTED") {
-            // Timeout
-            return Promise.reject(new Error("Couldn't connect to server"));
-        }
+// axios.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem("token");
+//         if (token) config.headers.Authorization = `Bearer ${token}`;
+//         return config;
+//     },
+//     (error) => {
+//         // Do something with request error
+//         return Promise.reject(error);
+//     }
+// );
 
-        if (error.response && error.response.data) {
-            let response = error.response.data;
-            if (response.code) {
-                // Got a response code in body
-                if (response.code == 404) {
-                    return Promise.reject(new Error("404 Not found"));
-                }
-                console.log("Got API error: ", response);
-                return Promise.reject(response);
-            }
-            return Promise.reject(new Error(response.message));
-        }
-        return Promise.reject(error);
-    }
-);
+// axios.interceptors.response.use(
+//     (response) => {
+//         return response.data;
+//     },
+//     (error) => {
+//         if (error.code == "ECONNABORTED") {
+//             // Timeout
+//             return Promise.reject(new Error("Couldn't connect to server"));
+//         }
+
+//         if (error.response && error.response.data) {
+//             let response = error.response.data;
+//             if (response.code) {
+//                 // Got a response code in body
+//                 if (response.code == 404) {
+//                     return Promise.reject(new Error("404 Not found"));
+//                 }
+//                 console.log("Got API error: ", response);
+//                 return Promise.reject(response);
+//             }
+//             return Promise.reject(new Error(response.message));
+//         }
+//         return Promise.reject(error);
+//     }
+// );
+
+let BASE_URL = "https://mindfulbackend.azurewebsites.net/api"
+
+axios.defaults.baseURL = BASE_URL;
 
 export default axios;
