142402adcedfcf71a6f8129f51f307c142a251c6
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2024-01-01 21:06:28 +0530

    Final Commit

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,65 +1,62 @@
-import axios from "axios";
+import _axios from "axios";
 
-// if (import.meta.env.MODE != "development") {
-//     console.log("Is prod.");
-// } else {
-//     console.log("Is dev.");
-// }
+if (import.meta.env.MODE != "development") {
+    console.log("Is prod.");
+} else {
+    console.log("Is dev.");
+}
 
-// let BASE
 
-// const axios = _axios.create({
-//     baseURL:
-//         import.meta.env.MODE != "development"
-//             ? "/api"
-//             : "https://mindfulbackend.azurewebsites.net/api",
-//     timeout: 3000,
-//     headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//     },
-// });
 
-// axios.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem("token");
-//         if (token) config.headers.Authorization = `Bearer ${token}`;
-//         return config;
-//     },
-//     (error) => {
-//         // Do something with request error
-//         return Promise.reject(error);
-//     }
-// );
+const axios = _axios.create({
+    baseURL: "https://mindfulbackend.azurewebsites.net/api",
+    timeout: 3000,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
 
-// axios.interceptors.response.use(
-//     (response) => {
-//         return response.data;
-//     },
-//     (error) => {
-//         if (error.code == "ECONNABORTED") {
-//             // Timeout
-//             return Promise.reject(new Error("Couldn't connect to server"));
-//         }
+axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (error) => {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
 
-//         if (error.response && error.response.data) {
-//             let response = error.response.data;
-//             if (response.code) {
-//                 // Got a response code in body
-//                 if (response.code == 404) {
-//                     return Promise.reject(new Error("404 Not found"));
-//                 }
-//                 console.log("Got API error: ", response);
-//                 return Promise.reject(response);
-//             }
-//             return Promise.reject(new Error(response.message));
-//         }
-//         return Promise.reject(error);
-//     }
-// );
+axios.interceptors.response.use(
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        if (error.code == "ECONNABORTED") {
+            // Timeout
+            return Promise.reject(new Error("Couldn't connect to server"));
+        }
 
-let BASE_URL = "https://mindfulbackend.azurewebsites.net/api"
+        if (error.response && error.response.data) {
+            let response = error.response.data;
+            if (response.code) {
+                // Got a response code in body
+                if (response.code == 404) {
+                    return Promise.reject(new Error("404 Not found"));
+                }
+                console.log("Got API error: ", response);
+                return Promise.reject(response);
+            }
+            return Promise.reject(new Error(response.message));
+        }
+        return Promise.reject(error);
+    }
+);
 
-axios.defaults.baseURL = BASE_URL;
+// let BASE_URL = "https://mindfulbackend.azurewebsites.net/api"
+
+// axios.defaults.baseURL = BASE_URL;
 
 export default axios;
